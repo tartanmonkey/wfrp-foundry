0a13eae69e67d2b16f7c9aece57a45ada3d2fda4
dave dow <dave@madmunkigames.com>
2022-11-24 08:22:29 +0000

    added outputting combat & magic skills first

--- character_creator.py
+++ character_creator.py
@@ -455,10 +455,25 @@ class GameCharacter:
 
         return output
 
-    def get_skills_output(self):
+    def get_skills_output_legacy(self):
         output = "\n-----SKILLS----------------\n"
         index = 0
         for skill, value in self.skills.items():
+            if index != 0 and index % 3 == 0:
+                output += "\n"
+            output += f"{skill}: {self.get_skill_total(skill)}, " #call to get skill total here
+            index += 1
+        return output
+
+    def get_skills_output(self):
+        # add combat & magic skills first
+        ordered_skills = {key: value for key, value in self.skills.items() if "Melee" in key or "Ranged" in key or "Dodge" in key or "Channelling" in key or "Language (Magick)" in key or "Pray" in key}
+        for skill, value in self.skills.items():
+            if skill not in ordered_skills:
+                ordered_skills[skill] = value
+        output = "\n-----SKILLS----------------\n"
+        index = 0
+        for skill, value in ordered_skills.items():
             if index != 0 and index % 3 == 0:
                 output += "\n"
             output += f"{skill}: {self.get_skill_total(skill)}, " #call to get skill total here
@@ -509,7 +524,6 @@ class GameCharacter:
                         value = get_random_skill_value(num_rolls)
                     # self.skills[skill] = value
                     self.advance_skill(skill, value)
-
 
     def advance_skill(self, skill, value):
         global skill_groups
